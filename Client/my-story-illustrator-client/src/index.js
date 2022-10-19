71c42e524942a8a4f7e23399e77356bb2e0e8c58
rougesteelproject <rougesteelproject@gmail.com>
2022-10-19 13:12:56 -0600

    added a comment

--- Client/my-story-illustrator-client/src/index.js
+++ Client/my-story-illustrator-client/src/index.js
@@ -6,6 +6,7 @@ import Header from './header/header';
 
 import {stories, justSignedIn, editingStory, pageTitle} from './constants';
 
+//This is just to show of the My Stories page and can be changed or ignored or whatever.
 const element = (
     <body>
         <Header justSignedIn = {justSignedIn} editingStory = {editingStory} pageTitle = {pageTitle}/>
