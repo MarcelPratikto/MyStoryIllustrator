2f884e1d06d4612cb5ad4af11ef469b7acdfdada
rougesteelproject <rougesteelproject@gmail.com>
2022-10-19 13:12:03 -0600

    images re-centered

--- Client/my-story-illustrator-client/src/my_stories/story_button_wrapper.js
+++ Client/my-story-illustrator-client/src/my_stories/story_button_wrapper.js
@@ -7,7 +7,7 @@ function StoryButtonWrapper(props){
     return(
 
         <WrapItem borderRadius="20px" shadow="lg" borderColor="blue" borderWidth="1px" borderStyle="solid" width={256} height={353} overflow="hidden">
-            <Center width={story_image_width} height={story_image_height + 10}>
+            <Center width={story_image_width} height={story_image_height + 53}>
                 {props.element}
             </Center>
             
