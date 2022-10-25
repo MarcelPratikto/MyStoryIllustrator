4c222e7754090f4373921cf866226543ff2691f2
Cierra Morris <4cibear@gmail.com>
2022-10-25 13:07:56 -0600

    update imports

--- Client/my-story-illustrator-client/src/pages/homepage.js
+++ Client/my-story-illustrator-client/src/pages/homepage.js
@@ -1,7 +1,7 @@
 import MyStories from '../components/my_stories/my_stories';
 import Header from '../components/header/header';
 
-import {stories, justSignedIn, editingStory, pageTitle} from './constants';
+import { stories, justSignedIn, editingStory, pageTitle } from '../util/constants';
 //These were for testing.
 //TODO this won't work to show off until we get states working and send these props here
 
