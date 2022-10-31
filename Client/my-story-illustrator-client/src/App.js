43afe86b0352f30ee1be1538069b7ac098ff740a
rougesteelproject <rougesteelproject@gmail.com>
2022-10-31 13:26:21 -0600

    book path pseudocode

--- Client/my-story-illustrator-client/src/App.js
+++ Client/my-story-illustrator-client/src/App.js
@@ -21,6 +21,9 @@ function App() {
           <Route path="/">
             <Homepage />
           </Route>
+          <Route path="/book">
+            <readBook />
+          </Route>
         </Switch>
       </Router>
     </ChakraProvider>
