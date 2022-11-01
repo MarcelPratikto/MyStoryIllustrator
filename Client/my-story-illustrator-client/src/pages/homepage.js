9076a8e812d04089a43692edfe2ef312a6c5bb4c
Daisy Nadauld <daisykunzler@gmail.com>
2022-11-01 13:03:04 -0600

    added imports for isloggedinatom

--- Client/my-story-illustrator-client/src/pages/homepage.js
+++ Client/my-story-illustrator-client/src/pages/homepage.js
@@ -1,11 +1,15 @@
 import MyStories from '../components/my_stories/my_stories';
 import Header from '../components/header/header';
+import {useAtom} from 'jotai';
+import { isLoggedInAtom } from '../../store/atoms';
+
 
 import { stories, justSignedIn, editingStory, pageTitle } from '../util/constants';
 //These were for testing.
 //TODO this won't work to show off until we get states working and send these props here
 
 function HomePage(props) {
+    const [isLoggedIn, updateIsLoggedIn] = useAtom(isLoggedInAtom);
     return(
         <>
             <Header justSignedIn = {props.justSignedIn} editingStory = {props.editingStory} pageTitle = {props.pageTitle}/>
