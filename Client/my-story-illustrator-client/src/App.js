f21cf2d33ba8d8a46fb166f86e41a16de08dbb62
Daisy Nadauld <daisykunzler@gmail.com>
2022-11-01 12:41:53 -0600

    Added Jotai and atom to control logged in state

--- Client/my-story-illustrator-client/src/App.js
+++ Client/my-story-illustrator-client/src/App.js
@@ -1,15 +1,18 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import LoginForm from './components/login/loginForm';
 import CreateAccForm from './components/login/createAccForm';
+import { Provider } from "jotai";
 
 function App() {
   return (
+    <Provider>
     <ChakraProvider>
       <div className="App">
         <LoginForm />
         <CreateAccForm />
       </div>
     </ChakraProvider>
+    </Provider>
   );
 }
 
