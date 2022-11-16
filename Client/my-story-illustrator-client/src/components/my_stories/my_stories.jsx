a5404bcd965a3c18e407ae3a3891101b432c2fc7
Cierra Morris <4cibear@gmail.com>
2022-11-15 18:42:44 -0700

    fixed unique key error

--- Client/my-story-illustrator-client/src/components/my_stories/my_stories.jsx
+++ Client/my-story-illustrator-client/src/components/my_stories/my_stories.jsx
@@ -11,17 +11,16 @@ function MyStories(props) {
     const stories = props.stories
     // TODO: make books a different colors
     // TODO: add subtle hover effect on the books
-
     return (
         <Wrap padding="20px" bg="#c6caef" flexGrow="1">
-            <StoryButtonWrapper bg="#e1e1ea">
+            <StoryButtonWrapper bg="#e1e1ea" >
                 <NewStoryButton/>
             </StoryButtonWrapper>
             
             { stories && stories.map((story) => {
                 return (
-                    <StoryButtonWrapper>
-                        <Link as={ReactRouterLink} to={'/book/' + story.id} key={story.id} display="block" h="100%" _hover={{textDecoration: "none"}} >
+                    <StoryButtonWrapper key={story.id}>
+                        <Link as={ReactRouterLink} to={'/book/' + story.id}  display="block" h="100%" _hover={{textDecoration: "none"}} >
                             <StoryCoverButton Title={story.title} CoverImageUri={story.coverUrl} />
                         </Link>
                     </StoryButtonWrapper>
