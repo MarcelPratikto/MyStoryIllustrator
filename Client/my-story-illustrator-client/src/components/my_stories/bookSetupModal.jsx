57c12ca9448dc6e7a39fd28deee46ab74409cf9f
rougesteelproject <rougesteelproject@gmail.com>
2022-11-28 12:56:27 -0700

    moved bSM, trying to reverse engineer login

--- Client/my-story-illustrator-client/src/components/my_stories/bookSetupModal.jsx
+++ Client/my-story-illustrator-client/src/components/my_stories/bookSetupModal.jsx
@@ -12,11 +12,49 @@ import {
     FormControl,
     Input
 } from "@chakra-ui/react";
-import { useEffect } from "react";
-import StyleChoices from "./styleChoices";
+import React, { useRef, useEffect } from 'react';
+import useHttp from '../../util/use-http';
+import {useAtom} from 'jotai';
+import { userTokenAtom, userIdAtom } from '../../store/atoms';
+import { Link as ReactRouterLink, useNavigate } from 'react-router-dom';
+
+import StyleChoices from "../read_book/styleChoices";
 
 function BookSetupModal() {
+    const [userToken, setUserToken] = useAtom(userTokenAtom);
+    const navigate = useNavigate();
     const { isOpen, onOpen, onClose } = useDisclosure()
+    const {error, sendRequest } = useHttp();
+    const titleInputRef = useRef();
+    const authorInputRef = useRef();
+    const styleInputRef = useRef();
+
+    const saveBook = () => {
+        const request = {
+          title: titleInputRef.current.value,
+          author: authorInputRef.current.value,
+          style: styleInputRef.current.value
+        } 
+        sendRequest({
+          url: 'http://localhost:8080/my_stories',
+          //TODO is this right? /\
+          method: 'POST',
+          body: request,
+          headers: {
+            "Content-Type": "application/json",
+            "Authorization": userToken
+          }
+          //TODO if the userToken's deleted/ the person's logged out, does the book take them back to login?
+        }, response => {
+          if (!error) {
+            navigate("/read_book/{response.book_id}");
+            //TODO navigate where? /\
+          } else {
+            console.error(error)
+          }
+        })
+    }
+
     useEffect(() => {
         onOpen()
 
@@ -47,7 +85,7 @@ function BookSetupModal() {
                     </ModalBody>
 
                     <ModalFooter>
-                        <Button colorScheme='blue' mr={3} onClick={onClose}>
+                        <Button colorScheme='blue' mr={3} onClick={saveBook}>
                             Go
                         </Button>
                     </ModalFooter>
--- Client/my-story-illustrator-client/src/components/my_stories/new_story_button.jsx
+++ Client/my-story-illustrator-client/src/components/my_stories/new_story_button.jsx
@@ -1,11 +1,12 @@
 import { Heading, VStack, Center, Icon } from '@chakra-ui/react';
 import { BsPlusCircle } from 'react-icons/bs';
+import BookSetupModal from './bookSetupModal';
 
 function NewStoryButton() {
 
     return (
         <>
-            <VStack border="none" background="none">
+            <VStack border="none" background="none" onClick={BookSetupModal.onOpen}>
                 <Heading size="lg">New Story</Heading>
                 <Center>
                     <Icon as={BsPlusCircle} boxSize={24} />
