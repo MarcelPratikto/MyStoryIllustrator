9bbed320283e876884df13c7bd9ac7d241ee90ec
Daisy Nadauld <daisykunzler@gmail.com>
2022-11-28 12:55:29 -0700

    user setting button work

--- Client/my-story-illustrator-client/src/components/header/user_settings_button.jsx
+++ Client/my-story-illustrator-client/src/components/header/user_settings_button.jsx
@@ -1,10 +1,6 @@
 import { IconButton } from "@chakra-ui/react";
 import { BsGearFill } from "react-icons/bs";
 import React from "react";
-import BookSetupModal from "../read_book/bookSetupModal";
-import { isModalOpenAtom } from '../../store/atoms';
-import { useAtom } from 'jotai';
-
 import {
     Button,
     Modal,
@@ -20,13 +16,33 @@ import {
     Input
 } from "@chakra-ui/react";
 import { useEffect } from "react";
+import StyleChoices from "../read_book/styleChoices"
+import { useParams } from "react-router-dom"
+import { currentBookAtom } from "../../store/atoms";
+import { useAtom } from "jotai"
 
-function UserSettingsButton() {
+function UserSettingsButton(props) {
     const { isOpen, onOpen, onClose } = useDisclosure()
     useEffect(() => {
         onOpen()
 
     });
+
+    const [currentBook, setCurrentBook] = useAtom(currentBookAtom);
+
+    function HandleTitleChange(title) {
+        setCurrentBook({
+            ...currentBook, title: title
+        })
+    }
+
+    function HandleAuthorChange(author) {
+        setCurrentBook({
+            ...currentBook, author: author
+        })
+    }
+
+   // const {getinfo, updateInfo} = 
 
     return (
         <>
@@ -35,26 +51,36 @@ function UserSettingsButton() {
             <Modal isOpen={isOpen} onClose={onClose} size="4xl">
                 <ModalOverlay />
                 <ModalContent>
-                    <ModalHeader>Setup</ModalHeader>
+                    <ModalHeader>Settings</ModalHeader>
                     <ModalCloseButton />
                     <ModalBody>
                         <FormControl>
-                            <FormLabel>Title</FormLabel>
-                            <Input type='text' />
+                            <FormLabel> Title </FormLabel>
+                            <Input type='text' placeholder="New Title Here" value={currentBook.title} onChange={HandleTitleChange}/>
                         </FormControl>
                         <FormControl>
                             <FormLabel>Author</FormLabel>
-                            <Input type='text' />
+                            <Input type='text' placeholder="New Author Here" value={currentBook.author} onChange={HandleAuthorChange}/>
                         </FormControl>
                         <FormControl>
-                            <FormLabel>Art Style</FormLabel>
-
+                            <FormLabel>Change Art Style</FormLabel>
+                            <StyleChoices/>
                         </FormControl>
                     </ModalBody>
-
                     <ModalFooter>
-                        <Button colorScheme='red' mr={3} onClick={onClose}>
+                        <Button
+                            isLoading
+                            loadingText='Deleting'
+                            colorScheme='red'
+                            mr={3} 
+                            onClick={onClose}
+                            variant='outline'
+                            spinnerPlacement='start'
+                        >
                             Delete Book
+                        </Button>
+                        <Button colorScheme='blue' mr={3} onClick={onClose}>
+                             Update
                         </Button>
                         <Button colorScheme='blue' mr={3} onClick={onClose}>
                              Close
